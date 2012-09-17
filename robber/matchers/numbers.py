8222787f47ecc0c5ddafa6d64572d46b259e0caf
Veselin Todorov <veselin@veselin.bg>
2012-09-17 12:33:39 +0300

    Add docs for numbers

--- robber/matchers/numbers.py
+++ robber/matchers/numbers.py
@@ -2,6 +2,9 @@ from robber import expect
 from base import Base
 
 class Above(Base):
+    """
+    expect(2).to.be.above(1)
+    """
     def matches(self):
         return self.actual > self.expected
 
@@ -9,6 +12,9 @@ class Above(Base):
         return 'Expected %d to be above %d' % (self.actual, self.expected)
 
 class Below(Base):
+    """
+    expect(1).to.be.below(2)
+    """
     def matches(self):
         return self.actual < self.expected
 
@@ -16,6 +22,9 @@ class Below(Base):
         return 'Expected %d to be below %d' % (self.actual, self.expected)
 
 class Within(Base):
+    """
+    expect(1).to.be.within(0, 2)
+    """
     def matches(self):
         return self.expected <= self.actual <= self.args[0]
 
