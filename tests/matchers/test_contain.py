eda1d37c697956c92cf930c640a9d8d1a382bea5
root <avlazarov@gmail.com>
2012-12-05 00:20:23 +0200

    Add tests for 'contain' matcher

---
+++ tests/matchers/test_contain.py
@@ -0,0 +1,22 @@
+import unittest
+from robber import expect
+from robber.matchers.contain import Contain
+
+class TestAbove(unittest.TestCase):
+    def test_matches(self):
+        expect(Contain({'key': 'value'}, 'key').matches()) == True
+        expect(Contain({1, 2, 3}, 1).matches()) == True
+        expect(Contain([1, 2, 3], 2).matches()) == True
+        expect(Contain((1, 2, 3), 3).matches()) == True
+
+        expect(Contain({'key': 'value'}, 'other').matches()) == False
+        expect(Contain({1, 2, 3}, 4).matches()) == False
+        expect(Contain([1, 2, 3], 4).matches()) == False
+        expect(Contain((1, 2, 3), 4).matches()) == False
+
+    def test_failure_message(self):
+        contain = Contain([1, 2, 3], 4)
+        expect(contain.failure_message()) == 'Expected {} to contain 4'.format([1, 2, 3])
+
+    def test_register(self):
+        expect(expect.matcher('contain')) == Contain
