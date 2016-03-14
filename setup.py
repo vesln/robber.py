bc75ce9ffc0616f3107353d3b85d739f15503308
Tao Liang <taoenator@gmail.com>
2016-03-14 10:43:52 -0700

    Adding Python 3 to the pypi classifiers.

--- setup.py
+++ setup.py
@@ -14,11 +14,13 @@ setup(
         'robber.matchers',
     ],
     classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Testing'
     ],
 )
