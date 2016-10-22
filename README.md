bd5b252ce875f7a879579cfb1edc20b39fe2d83f
East Agile <open-source@eastagile.com>
2016-10-22 16:46:29 +0700

    Cache raised, revised README.md

--- README.md
+++ README.md
@@ -251,7 +251,6 @@ Asserts that the target throws an exception
 
 ``` python
 expect(lambda: raise_exception(...)).to.throw(Exception)
-expect(lambda: raise_exception(...)).throws(Exception)
 expect(any_callable).to.throw(Exception)
 ```
 
--- robber/matchers/exception.py
+++ robber/matchers/exception.py
@@ -7,27 +7,28 @@ class ExceptionMatcher(Base):
     expect(lambda: call_something(with_some_params)).to.throw(any_exception)
     """
     @property
-    def _raised(self):
-        raised = None
-
-        if not callable(self.actual):
-            raise TypeError('{actual} is not callable'.format(actual=self.actual))
-
+    def raised(self):
         try:
-            self.actual()
-        except BaseException as raised:
-            return raised
+            return self._raised
+        except AttributeError:
+            if not callable(self.actual):
+                raise TypeError('{actual} is not callable'.format(actual=self.actual))
+
+            try:
+                self.actual()
+            except BaseException as raised:
+                self._raised = raised
+                return self._raised
 
     def matches(self):
-        return isinstance(self._raised, self.expected)
+        return isinstance(self.raised, self.expected)
 
     def failure_message(self):
-        if self._raised:
-            got = self._raised.__class__.__name__
+        if self.raised:
+            got = self.raised.__class__.__name__
         else:
             got = 'nothing'
 
         return 'Expected %s, got %s' % (self.expected.__name__, got)
 
 expect.register('throw', ExceptionMatcher)
-expect.register('throws', ExceptionMatcher)
