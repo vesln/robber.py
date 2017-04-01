c9f5d2d18bfa7570cb5edf06528a952092b48ee7
East Agile <open-source@eastagile.com>
2017-04-02 03:43:01 +0700

    [f] Refactor boolean tests
    - Remove redundant test
    - Split boolean interaction tests into smaller ones

--- tests/integrations/test_boolean.py
+++ tests/integrations/test_boolean.py
@@ -4,7 +4,7 @@ from robber import expect
 from tests import must_fail
 
 
-class TestBooleanIntegrations(TestCase):
+class TestTrueMatcherIntegrations(TestCase):
     def test_true_success(self):
         expect(True).to.be.true()
 
@@ -12,19 +12,21 @@ class TestBooleanIntegrations(TestCase):
     def test_true_failure(self):
         expect(False).to.be.true()
 
+    def test_not_true_success(self):
+        expect(False).not_to.be.true()
+
+    @must_fail
+    def test_not_true_failure(self):
+        expect(True).not_to.be.true()
+
+
+class TestFalseMatcherIntegrations(TestCase):
     def test_false_success(self):
         expect(False).to.be.false()
 
     @must_fail
     def test_false_failure(self):
         expect(True).to.be.false()
-
-    def test_not_true_success(self):
-        expect(False).not_to.be.true()
-
-    @must_fail
-    def test_not_true_success(self):
-        expect(True).not_to.be.true()
 
     def test_not_false_success(self):
         expect(True).not_to.be.false()
