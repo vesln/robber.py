56743baff6ead8306e1446ce0a77c1b5128f1520
East Agile <open-source@eastagile.com>
2017-04-05 00:31:43 +0700

    [r] Add support for length expectations
    - not_to.have.length
    - not_to.be.empty

--- tests/matchers/test_length.py
+++ tests/matchers/test_length.py
@@ -1,6 +1,7 @@
 import unittest
+
 from robber import expect
-from robber.matchers.length import Length, Empty, NotEmpty
+from robber.matchers.length import Length, Empty
 
 
 class TestLength(unittest.TestCase):
@@ -16,6 +17,10 @@ class TestLength(unittest.TestCase):
     def test_failure_message(self):
         length = Length('foo', 2)
         expect(length.failure_message()) == 'Expected "foo" to have a length of 2'
+
+    def test_failure_message_with_not_to(self):
+        length = Length('foo', 3, is_negative=True)
+        expect(length.failure_message()) == 'Expected "foo" not to have a length of 3'
 
     def test_register(self):
         expect(expect.matcher('length')) == Length
@@ -33,21 +38,9 @@ class TestEmpty(unittest.TestCase):
         empty = Empty('foo')
         expect(empty.failure_message()) == 'Expected "foo" to be empty'
 
+    def test_failure_message_with_not_to(self):
+        empty = Empty('', is_negative=True)
+        expect(empty.failure_message()) == 'Expected "" not to be empty'
+
     def test_register(self):
         expect(expect.matcher('empty')) == Empty
-
-
-class TestNotEmpty(unittest.TestCase):
-    def test_matches(self):
-        expect(NotEmpty('').matches()).to.eq(False)
-        expect(NotEmpty([]).matches()).to.eq(False)
-
-        expect(NotEmpty('foo').matches()).to.eq(True)
-        expect(NotEmpty([1, 2]).matches()).to.eq(True)
-
-    def test_failure_message(self):
-        not_empty = NotEmpty('foo')
-        expect(not_empty.failure_message()) == 'Expected "foo" to be nonempty'
-
-    def test_register(self):
-        expect(expect.matcher('not_empty')) == NotEmpty
