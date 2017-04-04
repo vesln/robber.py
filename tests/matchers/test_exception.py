66314c87ca2c691ca2c974128841c342cd32ecbf
East Agile <open-source@eastagile.com>
2017-04-05 01:29:22 +0700

    [r] Add support for not_to.throw

--- tests/matchers/test_exception.py
+++ tests/matchers/test_exception.py
@@ -18,9 +18,15 @@ class TestExceptionMatcher(unittest.TestCase):
     def test_failure_message_with_wrong_exception(self):
         exception_raised = ExceptionMatcher(lambda: self.raise_exception(TypeError()), ZeroDivisionError)
         message = exception_raised.failure_message()
-        expect(message) == 'Expected ZeroDivisionError, got TypeError'
+        expect(message) == 'Expected ZeroDivisionError to be raised, got TypeError'
+
+    def test_negative_failure_message_with_wrong_exception(self):
+        exception_raised = ExceptionMatcher(
+            lambda: self.raise_exception(ZeroDivisionError()), ZeroDivisionError, is_negative=True)
+        message = exception_raised.failure_message()
+        expect(message) == 'Expected ZeroDivisionError not to be raised, got ZeroDivisionError'
 
     def test_failure_message_with_no_exception_was_raised(self):
         no_exception = ExceptionMatcher(lambda: None, Exception)
         message = no_exception.failure_message()
-        expect(message) == 'Expected Exception, got nothing'
+        expect(message) == 'Expected Exception to be raised, got nothing'
