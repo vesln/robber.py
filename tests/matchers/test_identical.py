e321e68164bd8cfc3752efabfb4f3ea8fef958dc
East Agile <open-source@eastagile.com>
2017-04-05 01:07:54 +0700

    [f] Support not_to.equal
    - Remove to.not_equal

--- tests/matchers/test_identical.py
+++ tests/matchers/test_identical.py
@@ -1,5 +1,5 @@
 from robber import expect
-from robber.matchers.identical import Identical, NotIdentical
+from robber.matchers.identical import Identical
 
 
 class TestIdentical:
@@ -12,22 +12,10 @@ class TestIdentical:
         message = identical.failure_message()
         expect(message) == 'Expected "actual" to be "expected"'
 
+    def test_negative_failure_message(self):
+        identical = Identical('actual', 'actual', is_negative=True)
+        message = identical.failure_message()
+        expect(message) == 'Expected "actual" not to be "actual"'
+
     def test_register(self):
         expect(expect.matcher('equal')) == Identical
-
-
-class TestNotIdentical:
-    def test_matches(self):
-        expect(NotIdentical({0: 1}, {0: 1}).matches()).to.eq(True)
-        expect(NotIdentical(1, 1).matches()).to.eq(False)
-
-        expect(NotIdentical({0: 1}, {0: 1}).matches()).to.eq(True)
-        expect(NotIdentical(1, 1).matches()).to.eq(False)
-
-    def test_failure_message(self):
-        identical = NotIdentical('actual', 'expected')
-        message = identical.failure_message()
-        expect(message) == 'Expected "actual" not to be "expected"'
-
-    def test_register(self):
-        expect(expect.matcher('not_equal')) == NotIdentical
