7edfd73cadd777d808f3a732d20c98262f811285
East Agile <open-source@eastagile.com>
2017-04-13 23:39:19 +0700

    [r] Create a helper that store functions used to build strings

--- robber/matchers/called_with.py
+++ robber/matchers/called_with.py
@@ -1,6 +1,7 @@
 from mock import call
 
 from robber import expect
+from robber.helper import Helper
 from robber.matchers.base import Base
 
 
@@ -20,7 +21,7 @@ class CalledWith(Base):
             return 'Expected {actual}{negative_message} to be called with {expected_args}. ' \
                    'Actually not called.'.format(
                         actual=self.actual, negative_message=self.negative_message,
-                        expected_args=self.build_expected_params_string(
+                        expected_args=Helper.build_expected_params_string(
                             expected=self.expected, args=self.args, kwargs=self.kwargs
                         )
                     )
@@ -28,38 +29,11 @@ class CalledWith(Base):
         return 'Expected {actual}{negative_message} to be called with {expected_params}. ' \
                'Actually called with {called_params}.'.format(
                     actual=self.actual, negative_message=self.negative_message,
-                    expected_params=self.build_expected_params_string(
+                    expected_params=Helper.build_expected_params_string(
                         expected=self.expected, args=self.args, kwargs=self.kwargs
                     ),
-                    called_params=self.build_called_params_string(self.actual.call_args)
+                    called_params=Helper.build_called_params_string(self.actual.call_args)
                 )
-
-    @staticmethod
-    def build_expected_params_string(expected, args, kwargs):
-        if not expected:
-            expected_params_str = 'no arguments'
-        else:
-            expected_params_str = str(expected)
-            if args:
-                # Remove the parenthesis
-                args_str = str(args)[1:][:-1]
-                expected_params_str = ', '.join((expected_params_str, args_str))
-
-            if kwargs:
-                # Convert {'a':  1, 'b': 2} to 'a=1, b=2'
-                kwargs_str = ', '.join(['{0}={1!r}'.format(k, v) for k, v in kwargs.items()])
-                expected_params_str = ', '.join((expected_params_str, kwargs_str))
-
-        return expected_params_str
-
-    @staticmethod
-    def build_called_params_string(call_args):
-        if str(call_args) == 'call()':
-            called_params_str = 'no arguments'
-        else:
-            called_params_str = str(call_args)[5:][:-1]
-
-        return called_params_str
 
 
 expect.register('called_with', CalledWith)
--- tests/matchers/test_called_with.py
+++ tests/matchers/test_called_with.py
@@ -1,4 +1,3 @@
-import sys
 from unittest import TestCase
 
 from mock import Mock, call
@@ -70,56 +69,3 @@ class TestCalledWith(TestCase):
     def test_not_a_mock(self):
         self.assertRaises(TypeError, CalledWith("a", "b").matches)
         self.assertRaises(TypeError, CalledWith(1, "b").matches)
-
-
-class TestBuildCalledParamsString(TestCase):
-    def test_with_no_params(self):
-        call_args = call()
-        call_args_str = CalledWith.build_called_params_string(call_args)
-
-        expect(call_args_str).to.eq('no arguments')
-
-    def test_with_args_and_kwargs(self):
-        call_args = call(1, 'a', 3, a=1, b=2)
-        call_args_str = CalledWith.build_called_params_string(call_args)
-
-        expect(call_args_str).to.eq("1, 'a', 3, a=1, b=2")
-
-
-class TestBuildExpectedParamsString(TestCase):
-    def test_with_no_params(self):
-        expected_params_str = CalledWith.build_expected_params_string(expected=None, args=None, kwargs=None)
-        expect(expected_params_str).to.eq('no arguments')
-
-    def test_with_expected(self):
-        expected_params_str = CalledWith.build_expected_params_string(expected=1, args=None, kwargs=None)
-        expect(expected_params_str).to.eq('1')
-
-    def test_with_expected_and_args(self):
-        expected_params_str = CalledWith.build_expected_params_string(expected=1, args=(2, 'a'), kwargs=None)
-        expect(expected_params_str).to.eq("1, 2, 'a'")
-
-    def test_with_expected_and_kwargs(self):
-        expected_params_str = CalledWith.build_expected_params_string(
-            expected=1, args=None, kwargs={'a': 'a', 'one': 1}
-        )
-        # Since we cannot be sure about the order of a dict, which leads to the situation that the function
-        # build_expected_params_string can return different results (still acceptable). In order to test, we will split
-        # the result into smaller parts, and assert each one.
-        split_expected_params_str = expected_params_str.split(', ')
-
-        expect(split_expected_params_str).to.contain('1')
-        expect(split_expected_params_str).to.contain("a='a'")
-        expect(split_expected_params_str).to.contain('one=1')
-
-    def test_with_expected_and_args_and_kwargs(self):
-        expected_params_str = CalledWith.build_expected_params_string(
-            expected=1, args=(2, 'a'), kwargs={'b': 'b', 'one': 1}
-        )
-        split_expected_params_str = expected_params_str.split(', ')
-
-        expect(split_expected_params_str).to.contain('1')
-        expect(split_expected_params_str).to.contain('2')
-        expect(split_expected_params_str).to.contain("'a'")
-        expect(split_expected_params_str).to.contain("b='b'")
-        expect(split_expected_params_str).to.contain('one=1')
