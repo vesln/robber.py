a4c146470a1c21bacb890865b33932368e81665b
East Agile <open-source@eastagile.com>
2017-05-31 15:26:34 +0700

    [f] Add throw_exactly chain
    - Asserts that the target throws exactly an exception (not its parent)
    - expect(lambda).to.throw_exactly(exception)

--- tests/matchers/test_exception.py
+++ tests/matchers/test_exception.py
@@ -1,15 +1,20 @@
-import unittest
+from unittest import TestCase
 
 from robber import expect
-from robber.matchers.exception import ExceptionMatcher
+from robber.matchers.exception import ExceptionMatcher, ExactExceptionMatcher
 
 
-class TestExceptionMatcher(unittest.TestCase):
+class TestExceptionMatcher(TestCase):
     def raise_exception(self, ex):
         raise ex
 
+    def test_verb(self):
+        exception = ExceptionMatcher(lambda: 1 / 1)
+        expect(exception.verb).to.eq('be raised')
+
     def test_matches(self):
         expect(ExceptionMatcher(lambda: 1 / 0, ZeroDivisionError).matches()).to.eq(True)
+        expect(ExceptionMatcher(lambda: 1 / 0, Exception).matches()).to.eq(True)
         expect(ExceptionMatcher(lambda: None, Exception).matches()).to.eq(False)
 
     def test_actual_is_not_callable(self):
@@ -44,3 +49,52 @@ Z = 'nothing'
 Expected A to be raised
 Actually raised Z
 """
+
+    def test_register(self):
+        expect(expect.matcher('throw')) == ExceptionMatcher
+
+
+class TestExactExceptionMatcher(TestCase):
+    def raise_exception(self, ex):
+        raise ex
+
+    def test_verb(self):
+        exception = ExactExceptionMatcher(lambda: 1 + 1)
+        expect(exception.verb).to.eq('be exactly raised')
+
+    def test_matches(self):
+        expect(ExactExceptionMatcher(lambda: 1 / 0, ZeroDivisionError).matches()).to.eq(True)
+        expect(ExactExceptionMatcher(lambda: 1 / 0, Exception).matches()).to.eq(False)
+
+    def test_explanation_message_with_wrong_exception(self):
+        exception_raised = ExactExceptionMatcher(lambda: self.raise_exception(TypeError()), ZeroDivisionError)
+        message = exception_raised.explanation.message
+        expect(message) == """
+A = 'ZeroDivisionError'
+Z = 'TypeError'
+Expected A to be exactly raised
+Actually exactly raised Z
+"""
+
+    def test_negative_explanation_message_with_wrong_exception(self):
+        exception_raised = ExactExceptionMatcher(
+            lambda: self.raise_exception(ZeroDivisionError()), ZeroDivisionError, is_negative=True)
+        message = exception_raised.explanation.message
+        expect(message) == """
+A = 'ZeroDivisionError'
+Expected A not to be exactly raised
+But it happened
+"""
+
+    def test_explanation_message_with_no_exception_was_raised(self):
+        no_exception = ExactExceptionMatcher(lambda: None, Exception)
+        message = no_exception.explanation.message
+        expect(message) == """
+A = 'Exception'
+Z = 'nothing'
+Expected A to be exactly raised
+Actually exactly raised Z
+"""
+
+    def test_register(self):
+        expect(expect.matcher('throw_exactly')) == ExactExceptionMatcher
