35dc73f0149d83f2f7f0cae9e602b1aaa399f2c5
East Agile <open-source@eastagile.com>
2017-08-02 10:34:06 +0700

    [c] Deal with MD and RST doc

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 from setuptools import setup
+
+long_description = 'BDD / TDD assertion library for Python',
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 setup(
     name='robber',
-    version='1.1.1',
+    version='1.1.2',
     description='BDD / TDD assertion library for Python',
+    long_description=long_description,
     author='Tao Liang',
     author_email='tao@synapse-ai.com',
     url='https://github.com/vesln/robber.py',
